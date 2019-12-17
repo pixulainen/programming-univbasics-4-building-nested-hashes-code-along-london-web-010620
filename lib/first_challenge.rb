d3df01515f162b709e9cf10ebbb87d4ebdd3aff4
George Apetrei <george.apetrei@gmail.com>
2019-12-17 14:09:25 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,18 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague =>{
+       :partriach =>{
+         :name => {:name "" }
+       },
+       :matriarch =>{},
+       :hero => {},
+       :hero_friends =>[]
+     },
+     :capulet => {
+      :partriach =>{},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+     }
+   }
 end
